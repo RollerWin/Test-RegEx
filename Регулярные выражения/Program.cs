c65c644e30bd2fb81b8763831b2e6b03ad9d8b96
RollerWin <rinatyych@mail.ru>
2023-04-20 02:32:19 +0300

    Добавьте файлы проекта.

---
+++ Регулярные выражения/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Регулярные_выражения
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new string[]
+            {
+                "0",
+                "-6",
+                "-0",
+                "+2",
+                "-0.5",
+                "001",
+                "0.(35)1",
+                "0.(345)",
+                "0.0(64)",
+                "0.0",
+                "-3,750",
+                "-4.750"
+            };
+
+            Regex reg = new Regex("^(([-]?((([0]|([1-9][0-9]*))[.](([0-9]+[1-9])|((([0-9]*)([(][0-9]+[)])?)))|([1-9]+))))|[0])$");
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (reg.IsMatch(data[i]))
+                {
+                    Console.WriteLine(data[i]);
+                }
+            }
+        }
+    }
+}
